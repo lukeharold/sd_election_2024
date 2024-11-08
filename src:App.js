2f10b77e529c0ee656157d44598a379d266dbac2
Luke Harold <48370198+lukeharold@users.noreply.github.com>
2024-11-07 21:28:21 -0800

    Add files via upload

---
+++ src:App.js
@@ -0,0 +1,11 @@
+import ElectionDonutCharts from './components/ElectionDonutCharts';
+
+function App() {
+  return (
+    <div className="p-8">
+      <ElectionDonutCharts />
+    </div>
+  );
+}
+
+export default App;
